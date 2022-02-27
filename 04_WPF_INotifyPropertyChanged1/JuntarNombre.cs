115daf3fd1c7fb16acc8ed084403d9d8ed6d013e
sebastian.olarte <sebastian.olarte@local.vivaair.com>
2022-02-26 19:10:37 -0500

    Proyecto_04_WPF_INotifyPropertyChanged

---
+++ 04_WPF_INotifyPropertyChanged1/JuntarNombre.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _04_WPF_INotifyPropertyChanged1
+{
+    public class JuntarNombre: INotifyPropertyChanged
+    {
+        private string nombre, apellido, nombreCompleto;
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        //ESTE SERÁ EL METODO QUE SE DESENCADENE CADA VEZ QUE CAMBIE EL VALOR DE UNA PROPIEDAD DE CUALQUIERA DE LOS COMPONENTES
+        private void OnPropertyChanged(string property)
+        {
+            if (PropertyChanged != null)
+            {
+                PropertyChanged(sender: this, e: new PropertyChangedEventArgs(propertyName: property));
+            }
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+
+            set
+            {
+                nombre = value;
+                OnPropertyChanged("NombreCompleto");
+            }
+        }
+
+        public string Apellido
+        {
+            get { return apellido; }
+
+            set
+            {
+                apellido = value;
+                OnPropertyChanged("NombreCompleto");
+            }
+        }
+
+        public string NombreCompleto
+        {
+            get
+            {
+                nombreCompleto = Nombre + " " + Apellido;
+                return nombreCompleto;
+            }
+
+            set { }
+        }
+    }
+}
---
+++ 04_WPF_INotifyPropertyChanged1/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _04_WPF_INotifyPropertyChanged1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// EN ESTA APLICACION SE VERÁ EL USO DE LA INTERFAZ INotifyPropertyChanged
+    /// ESTA INTERFAZ ES CAPAZ DE REUNIR TODOS LOS EVENTOS QUE PUEDE DESENCADENAR UN OBJETO EN UNO SOLO. LO CUAL ES MUY UTIL SI NECESITAMOS
+    /// DETECTAR ALGÚN CAMBIO EN ALGUNA PROPIEDAD DEL OBJETO EN CUESTIÓN Y ASÍ PROCEDER A REALIZAR ALGUNA ACCIÓN.
+    /// ESTA INTERFAZ PERMITE NOTIFICAR QUE HA HABIDO UN CAMBIO EN UNA PROPIEDAD EN CONCRETO, SIN NECESIDAD DE CREAR UN METODO ESPECIFICO 
+    /// QUE SE ENCARGARÍA DE MANEJAR EL EVENTO DE CAMBIO EN ESA PROPIEDAD.
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            juntaNombreyApellido = new JuntarNombre { Nombre = "SEBASTIÁN",  Apellido = "OLARTE"};
+
+            this.DataContext = juntaNombreyApellido;
+        }
+
+        public JuntarNombre juntaNombreyApellido;
+    }
+}
